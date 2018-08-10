78303c9e7ee76079c4d0ad784df2c2b6c0320cec
Leif Eric Fredheim <463422+irleif@users.noreply.github.com>
2018-08-10 20:52:01 +0200

    Super naive and procedural implementation.

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -12,7 +12,21 @@ defmodule Fizzbuzz do
       :world
 
   """
-  def hello do
-    :world
+  def run do
+    fizzbuzz = fn n1, n2, n3 ->
+      case {n1, n2, n3} do
+        {0, 0, _} -> "FizzBuzz"
+        {0, _, _} -> "Fizz"
+        {_, 0, _} -> "Buzz"
+        {_, _, _} -> n3
+      end
+    end
+
+    result =
+      for n <- 1..100 do
+        fizzbuzz.(rem(n, 3), rem(n, 5), n)
+      end
+
+    Enum.map(result, fn n -> IO.puts(n) end)
   end
 end
